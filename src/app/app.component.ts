381769a944cd7dd6dafe442c0c098a3aab0ba78f
Mikael Medeiros <mikaeltruth@gmail.com>
2023-11-12 01:53:32 -0300

    Alteraçao no layout e textos de orçamento gerado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Clipboard } from '@angular/cdk/clipboard';
 @Component({
@@ -7,26 +7,48 @@ import { Clipboard } from '@angular/cdk/clipboard';
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'orc-tatu';
   generatedBudget = '';
 
   constructor(
     private fb: FormBuilder,
     private clipboard: Clipboard   
-  ) {};
+  ) {}
+
+  categories: any[] = [];
+  details: any[] = [];
+  
+  ngOnInit(): void {
+    this.categories = [
+      { name: 'Fineline', value: 'fineline' },
+      { name: 'Bold Line', value: 'bold line' },
+      { name: 'Realismo', value: 'realismo' },
+      { name: 'Old School', value: 'old school' },
+      { name: 'Black Work', value: 'black work' },
+    ];
+
+    this.details = [      
+      { name: 'Sombreamento', value: 'sombreamento' },
+      { name: 'Pontilhismo', value: 'pontilhismo' },
+      { name: 'Linhas', value: 'linhas' },
+      { name: 'Colorido', value: 'colorido' },
+      { name: 'Tinta Branca', value: 'tinta branca' }
+    ];
+  }
+;
 
   text: string | undefined;
 
   budgetForm = this.fb.group({
     id: [''],
-    client: ['Fulano', Validators.required],    
-    draw: ['Borboleta', Validators.required],
-    valorcm: [10.00, Validators.required],
-    cm: [2.5, Validators.required],
-    category: ['FINELINE', Validators.required],
-    bodyLocal: ['Pescoço'],
-    details: ['SOMBREAMENTO']
+    client: ['', Validators.required],    
+    draw: ['', Validators.required],
+    valorcm: [22.50, Validators.required],
+    cm: [null, Validators.required],
+    category: [[], Validators.required],
+    bodyLocal: [''],
+    details: [[]]
   }) 
 
   generateBudget() {
@@ -37,21 +59,34 @@ export class AppComponent {
     let cm = this.budgetForm.get('cm')?.value;
     let valorcm = this.budgetForm.get('valorcm')?.value;
     if((typeof valorcm !== undefined && valorcm != null) && (typeof cm !== undefined && cm != null)) {      
-      let pixValue = valorcm * cm;
+      let pixValue = (valorcm * cm) + 10;
       let creditValue = pixValue + 20;
 
-      this.generatedBudget = `Oi ${this.budgetForm.get('client')?.value}`.concat(
-        ` sua tattoo ${this.budgetForm.get('category')?.value}`).concat(
-        ` de ${this.budgetForm.get('draw')?.value},`).concat(
-        ` com ${this.budgetForm.get('cm')?.value}cm,`).concat(
-        ` no ${this.budgetForm.get('bodyLocal')?.value}`).concat(
-        ` com detalhe em ${this.budgetForm.get('details')?.value}`).concat(
+      this.generatedBudget = `${this.budgetForm.get('client')?.value},`.concat(
+        ` sua tattoo ${this.getTextFormated(this.budgetForm.get('category')?.value)}`).concat(
+        ` ${this.budgetForm.get('draw')?.value}`).concat(
+        ` de aproximadamente ${this.budgetForm.get('cm')?.value}cm,`).concat(
+        ` no(a) ${this.budgetForm.get('bodyLocal')?.value}`).concat(
+        ` com detalhe em ${this.getTextFormated(this.budgetForm.get('details')?.value)}`).concat(
         ` fica no valor de R$${pixValue} no PIX`).concat(
         ` ou R$${creditValue} no Cartão de Crédito em até 3x.`);
 
       console.log('Pix: R$', pixValue);
       console.log('Crédito: R$', creditValue);
     }
+  }
+
+  getTextFormated(lista: string []| undefined | null): string | undefined | null {
+    console.log(lista);
+    if (typeof lista !== undefined && lista != null) {
+      const stringOrganizada = lista
+      .map((str: string) => str + ", ")
+      .join("")            
+    
+      return stringOrganizada.replace(/,\s*$/, " e");
+    }
+    
+    return "";
   }
 
   copyText() {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { ButtonModule } from 'primeng/button';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { SelectButtonModule } from 'primeng/selectbutton';
 
 
 @NgModule({
@@ -17,6 +19,8 @@ import { AppComponent } from './app.component';
     BrowserAnimationsModule,
     ReactiveFormsModule,
     InputTextModule,
+    InputNumberModule,
+    SelectButtonModule,
     CardModule,
     ButtonModule    
   ],
